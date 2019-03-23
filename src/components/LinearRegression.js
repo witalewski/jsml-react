5d00cec9e6957091b080b67d69a941b21b3236d0
Adam Witalewski <krzysztof.witalewski@gmail.com>
2019-03-23 09:53:12 +0100

    add nice colors to SLR

--- src/components/LinearRegression.js
+++ src/components/LinearRegression.js
@@ -10,51 +10,7 @@ import {
   Legend
 } from "recharts";
 import { SLR } from "ml-regression";
-
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
-  }
-];
+import { colors } from "../utils/colors";
 
 export const LinearRegression = ({ data, seriesKey, labelKey }) => {
   let [X, y] = data.reduce(
@@ -94,13 +50,13 @@ export const LinearRegression = ({ data, seriesKey, labelKey }) => {
           <Line
             type="monotone"
             dataKey={seriesKey}
-            stroke="green"
+            stroke={colors[0]}
             dot={false}
           />
           <Line
             type="monotone"
             dataKey={predictionLabel}
-            stroke="red"
+            stroke={colors[1]}
             dot={false}
           />
         </LineChart>
